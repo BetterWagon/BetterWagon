f3008ca070aeea8a75d90433295ed1c19329fece
Shane Lx <39976362+shanefully-done@users.noreply.github.com>
2024-03-24 16:29:09 +0900

    Modularise the bot. Added room authorisation.

--- index.js
+++ index.js
@@ -1,67 +1,23 @@
 import * as dotenv from "dotenv";
 import { Server } from "@remote-kakao/core";
-import https from "follow-redirects/https.js";
+import { openAIChat } from "./plugins/openai-gpt.js"; // Importing openAIChat from openai-gpt.js
 
 dotenv.config({ path: "./.env" });
 console.log(process.env.OPENAI_API);
 const server = new Server({ useKakaoLink: false });
 
-const options = {
-	method: "POST",
-	hostname: "api.openai.com",
-	path: "/v1/chat/completions",
-	headers: {
-		"Content-Type": "application/json",
-		Authorization: process.env.OPENAI_API,
-	},
-	maxRedirects: 20,
-};
-
+// Handle incoming messages
 server.on("message", async (msg) => {
-	console.log("[" + msg.room + "] " + msg.sender.name + " : " + msg.content);
+	const authRoom = process.env.AUTH_ROOM.split(",");										// Authorise the room first
+	if (authRoom.includes(msg.room)) {
+		// console.log("[" + msg.room + "] " + msg.sender.name + " : " + msg.content);
+		// Add features for all authorised rooms
+		openAIChat(msg); // Using openAIChat from openai-gpt.js
+	}
 
-	switch (true) {
-		case msg.content.startsWith(">>"):
-			const allMsg = msg.content.slice(2);
-			const req = https.request(options, (res) => {
-				const chunks = [];
-
-				res.on("data", (chunk) => {
-					chunks.push(chunk);
-				});
-
-				res.on("end", () => {
-					const body = Buffer.concat(chunks);
-					const resExtract = JSON.parse(body.toString());
-					console.log("ChatGPT : " + resExtract.choices[0].message.content.trim());
-					msg.replyText(resExtract.choices[0].message.content.trim());
-				});
-
-				res.on("error", (error) => {
-					console.error(error);
-				});
-			});
-
-			const postData = JSON.stringify({
-				model: "gpt-3.5-turbo",
-				messages: [
-					{
-						role: "user",
-						content: allMsg,
-					},
-				],
-				temperature: 0.7,
-				user: msg.sender.name,
-			});
-
-			req.write(postData);
-			req.end();
-			break;
-		case msg.content.startsWith(">ping"):
-			msg.replyText("Pong!");
-			break;
-		default:
-			return;
+	// Parse ping request
+	if (msg.content.toLowerCase().startsWith("ping!")) {
+		msg.replyText("Pong!");
 	}
 });
 
